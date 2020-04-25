304e38cd8300ee41bc001b20e00694af3dca01b8
amdryzen53600 <maxtel.2002@gmail.com>
2020-04-25 21:26:16 +0300

    Implemented User, Admin classes

---
+++ Bank/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    // Admin = login + password
+    [Serializable]
+    class Admin
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Bank/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Models
+{
+    // Customer = login + password + passport data + deposits
+    [Serializable]
+    class Customer
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public uint AccountNumber { get; set; }
+        public string Address { get; set; }
+        public int BirthYear { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthDay { get; set; }
+    }
+}
