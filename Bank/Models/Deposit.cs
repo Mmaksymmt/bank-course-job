2671dd21a124bf1f4fd6d1875e7ab8c3a3eb1163
amdryzen53600 <maxtel.2002@gmail.com>
2020-04-27 21:20:23 +0300

    Implemented Deposit and TermDeposit classes

---
+++ Bank/Models/Deposit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Models
+{
+    // 
+    [Serializable]
+    class Deposit
+    {
+        public Deposit
+        (
+            int percent,
+            decimal value,
+            DateTime startDate
+        )
+        {
+            Percent = percent;
+            Value = value;
+            StartDate = startDate;
+        }
+
+        public int Percent { set; get; }
+        public decimal Value { set; get; }
+        public DateTime StartDate { set; get; }
+        //public Customer Owner;
+
+        protected Deposit() { }
+    }
+}
---
+++ Bank/Models/TermDeposit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Models
+{
+    // 
+    [Serializable]
+    class TermDeposit : Deposit
+    {
+        public TermDeposit
+        (
+            int percent,
+            decimal value,
+            DateTime startDate, 
+            DateTime expirationDate
+        )
+        {
+            Percent = percent;
+            Value = value;
+            StartDate = startDate;
+            ExpirationDate = expirationDate;
+        }
+
+        public DateTime ExpirationDate { get; set; }
+    }
+}
